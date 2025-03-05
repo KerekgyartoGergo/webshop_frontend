f44413227af8cd77edc3120d6ce95c911c756deb
KerekgyartoGergo <kerekgyartogergo05@gmail.com>
2025-03-05 10:43:27 +0100

    kép nagyítás

--- js/teszt.js
+++ js/teszt.js
@@ -230,9 +230,9 @@ async function addToCart(product_id, quantity = 1) {
 
 
 
-let img =document.getElementById("img");
+let img = document.getElementById("img");
 
 img.onmousemove = function(e) {
-    e.target.style.setProperty('--x', (100*e.offsetX/e.target.offsetWidth)+'%');
-    e.target.style.setProperty('--y', (100*e.offsetY/e.target.offsetHeight)+'%');
+  e.target.style.setProperty('--x',(100*e.offsetX/e.target.offsetWidth)+'%');
+  e.target.style.setProperty('--y',(100*e.offsetY/e.target.offsetHeight)+'%'); 
 }
