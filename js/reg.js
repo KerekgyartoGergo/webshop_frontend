a8426631e3975d3d8cec0fd1352dafae2e610f4b
KerekgyartoGergo <kerekgyartogergo05@gmail.com>
2025-03-13 12:57:10 +0100

    reg.html sweetalert

--- js/reg.js
+++ js/reg.js
@@ -11,45 +11,65 @@ document.addEventListener('keydown', function(event){
 
 
 async function register(){
-    const email =document.getElementById('email').value;
-    const user_name =document.getElementById('name').value;
-    const psw =document.getElementById('psw').value;
-    const psw2 =document.getElementById('psw2').value;
+    const email = document.getElementById('email').value;
+    const user_name = document.getElementById('name').value;
+    const psw = document.getElementById('psw').value;
+    const psw2 = document.getElementById('psw2').value;
 
+    console.log(email, user_name, psw, psw2);
 
-    console.log(email, name, psw, psw2);
-
-
-
-    if (psw!==psw2) {
-        return alert('A két jelszó nem eggyezik!!')
+    if (psw !== psw2) {
+        // alert('A két jelszó nem eggyezik!!')
+        return Swal.fire({
+            title: "A két jelszó nem eggyezik!",
+            icon: "error",
+            theme: 'dark'
+        });
     }
 
-
-    const res =await fetch('/api/register', {
+    const res = await fetch('/api/register', {
         method: 'POST',
-        headers:{
+        headers: {
             'content-type': 'application/json'
         },
-        body: JSON.stringify({user_name, email, psw})
+        body: JSON.stringify({ user_name, email, psw })
     });
-
 
     const data = await res.json();
     console.log(data);
 
     if (res.ok) {
-        alert(data.message);
-        window.location.href ='../login.html';
-    } else if(data.errors){
-        let errorMessage ='';
-        for (let i = 0; i < data.errors.length; i++){
+        // alert(data.message);
+        Swal.fire({
+            title: data.message,
+            icon: "success",
+            theme: 'dark'
+        });
+        window.location.href = '../login.html';
+    } else if (data.errors) {
+        let errorMessage = '';
+        for (let i = 0; i < data.errors.length; i++) {
             errorMessage += `${data.errors[i].error}\n`
         }
-        alert(errorMessage);
-    }else if(data.error){
-        alert(data.error);
-    }else {
-        alert('ismeretlen hiba')
+        // alert(errorMessage);
+        Swal.fire({
+            title: errorMessage,
+            icon: "error",
+            theme: 'dark'
+        });
+    } else if (data.error) {
+        // alert(data.error);
+        Swal.fire({
+            title: data.error,
+            icon: "error",
+            theme: 'dark'
+        });
+    } else {
+        // alert('ismeretlen hiba');
+        Swal.fire({
+            title: 'Ismeretlen hiba',
+            icon: "error",
+            theme: 'dark'
+        });
     }
 }
